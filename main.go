556591f0c450ab04f2f1787e2805339cd8cfaeea
Sergey <hi.sergeyl@gmail.com>
2023-08-17 13:25:20 +0300

    add switch

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"unicode"
 )
 
 var file, err = os.Open("text.txt")
@@ -22,10 +23,30 @@ func init() {
 	scanner.Split(bufio.ScanWords)
 }
 
+func countLetters(str string) int {
+	count := 0
+	for _, letter := range str {
+		if unicode.IsLetter(letter) {
+			count++
+		}
+	}
+	return count
+}
+
 func Highlight(w http.ResponseWriter, r *http.Request) {
 	chText := "<h1>"
 	for scanner.Scan() {
-		chText += fmt.Sprintf("<span style='color: black'>%s</span><span style='color: gray'>%s</span> ", scanner.Text()[:1], scanner.Text()[1:])
+		length := countLetters(scanner.Text())
+		switch {
+		case length < 4:
+			chText += fmt.Sprintf("<span style='color: black'>%s</span><span style='color: gray'>%s</span> ", scanner.Text()[:1], scanner.Text()[1:])
+		case length == 4:
+			chText += fmt.Sprintf("<span style='color: black'>%s</span><span style='color: gray'>%s</span> ", scanner.Text()[:2], scanner.Text()[2:])
+		case length <= 6:
+			chText += fmt.Sprintf("<span style='color: black'>%s</span><span style='color: gray'>%s</span> ", scanner.Text()[:3], scanner.Text()[3:])
+		default:
+			chText += fmt.Sprintf("<span style='color: black'>%s</span><span style='color: gray'>%s</span> ", scanner.Text()[:4], scanner.Text()[4:])
+		}
 	}
 	chText += "</h1>"
 	w.Header().Set("Content-Type", "text/html")
