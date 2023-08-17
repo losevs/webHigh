f2853afd64241701ac52f80547fc76c3a0dec03a
Sergey <hi.sergeyl@gmail.com>
2023-08-17 13:09:30 +0300

    .txt

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"log"
 	"net/http"
+	"os"
 )
+
+var file, err = os.Open("text.txt")
+
+func init() {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+var scanner = bufio.NewScanner(file)
+
+func init() {
+	scanner.Split(bufio.ScanWords)
+}
 
 func Highlight(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello."))
